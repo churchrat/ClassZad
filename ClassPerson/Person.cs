ed7dc73b2e5d85d3e8ec730d39babd509df220d4
churchrat <mihailkrustev08@gmail.com>
2024-04-09 22:15:25 +0300

    mm

---
+++ ClassPerson/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPerson
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public void Eat()
+        {
+            Console.WriteLine(this.name + " papam");
+        }
+    }
+}
---
+++ ClassPerson/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace ClassPerson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type personType = typeof(Person);
+            FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(fields.Length);
+
+            Person pesho = new Person();
+            pesho.name = "Pesho";
+            pesho.age = 20;
+            pesho.Eat();
+            Person gosho = new Person();
+            gosho.name = "Gosho";
+            gosho.age = 18;
+            gosho.Eat();
+            Person stamat = new Person();
+            stamat.name = "Stamata";
+            stamat.age = 43;
+            stamat.Eat();
+        }
+    }
+}
